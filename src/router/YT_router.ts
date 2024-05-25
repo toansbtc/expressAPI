e272d8eaca9a783f530b20dbe2972c2bf74576eb
kyo.loan <kyo.loan@freetrend.com.vn>
2024-05-25 16:15:13 +0700

    commit file

--- src/router/YT_router.ts
+++ src/router/YT_router.ts
@@ -1,8 +1,8 @@
 import express from "express"
 import YoutubeMp3Downloader from "youtube-mp3-downloader";
-import RNFS from "react-native-fs"
+import fs from "fs"
 import YTDL from "yt-get"
-import base64TOFile from "base64-to-file"
+import decode from "decode-base64";
 
 const app = express.Router();
 
@@ -13,16 +13,35 @@ app.post("/download", (req, res, next) => {
 
     YTDL.getVideoMP3Base64(videoURL).then((data => {
         const { base64, title } = data;
-        base64TOFile.convert(base64, "./src", ['mp3', 'mp4', 'WAV', 'WMA'], (file_path: any) => {
-            console.log(file_path)
-        })
-        res.send(base64);
+        // base64TOFile.convert(base64, "./src", ['mp3', 'mp4', 'WAV', 'WMA', 'webm'], (file_path: any) => {
+        //     console.log(file_path)
+        // })
+
+        // const binary = atob(base64);
+        // const u8 = new Uint8Array(binary.length);
+
+        // for (let i = 0, l = binary.length; i < l; i++) {
+
+        //     u8[i] = binary.charCodeAt(i);
+
+        // }
+        // const base64String = u8.reduce((data, byte) => data + String.fromCharCode(byte), '');
+
+        const binaryData = Buffer.from(base64, 'base64');
+
+        //fs.writeFileSync("./src/audio.mp3", binaryData);
+        res.send(binaryData);
     })).catch((error) => {
         console.error(error);
         res.send(error)
     })
 
 
+})
+
+app.get('/get_KEY', (req, res, next) => {
+    const dataKEY = process.env.YT_KEY;
+    res.send(dataKEY);
 })
 
 export default app;
